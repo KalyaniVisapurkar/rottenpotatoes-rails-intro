26ac0e5f8683cf2a552bdcc097f0278f14301068
Kalyani Visapurkar <kalyani.visapurkar@gmail.com>
2016-09-12 02:29:51 +0000

    First commit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+    def fooimage(image_path, size)
+        if size == "large"
+            actual_size = "1000x1000"
+        elsif size == "medium"
+            acutual_size = "100x100"
+        else
+            actual_size = "50x50"
+        end
+    image_tag image_path, size: "#{actual_size}"
+    end
 end
